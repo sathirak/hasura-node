c16ec0cefcf55fa4e02d98eeb014b5c17e3c7356
sekisaii <145209193+sekisaii@users.noreply.github.com>
2024-01-03 22:48:55 +0530

    yoga install works now

---
+++ app.js
@@ -0,0 +1,23 @@
+const { createServer } = require('http');
+const { createSchema, createYoga } = require('graphql-yoga');
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'Hello from Yoga!'
+      }
+    }
+  })
+})
+
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql')
+})
